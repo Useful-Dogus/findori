c6468f2aa34098b8cad879d92909805bb52554e9
chanhee-park <chanhee13p@gmail.com>
2026-03-20 15:50:56 +0900

    feat: implement public feed card renderer

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,7 +1,9 @@
+import FeedDisclaimer from '@/components/features/feed/FeedDisclaimer'
+
 export default function PublicLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="mx-auto min-h-screen max-w-[960px]">
-      {/* TODO(#24): 투자 자문 아님 고지 배너 */}
+      <FeedDisclaimer />
       <main>{children}</main>
     </div>
   )
